7e5b97c20f3176d5ef4c076bd8cdf4fda0c1d3e9
Adam Christie <fractos@gmail.com>
2019-05-30 13:39:58 +0100

    adding diagnostics option to XsltViewBehaviour to investigate problems on .net core.

--- Inversion.Web/Behaviour/View/XsltViewBehaviour.cs
+++ Inversion.Web/Behaviour/View/XsltViewBehaviour.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Reflection;
 using System.Text;
 using System.Xml;
 using System.Xml.Xsl;
@@ -8,13 +9,14 @@ using System.Xml.Xsl;
 using Inversion.Data;
 using Inversion.Process;
 using Inversion.Process.Behaviour;
+using log4net;
 
 namespace Inversion.Web.Behaviour.View {
 
 	/// <summary>
 	/// A behaviour that will transform the last view step by attempting to find
 	/// an appropriate XSL style sheet, based upon the context params
-	/// of *area*, *concern*, and *action*. 
+	/// of *area*, *concern*, and *action*.
 	/// </summary>
 	/// <remarks>
 	/// This is intended for use in Web application, not as a general
@@ -23,12 +25,14 @@ namespace Inversion.Web.Behaviour.View {
 
 	public class XsltViewBehaviour : ProcessBehaviour {
 
-		// This is a piece of voodoo I was handed by a friend who had some
-		//		similar occasional encoding problems which apparently are
-		//		due to an underlying problem in the BCL.
-		// Find out if its still relevant and what exactly its doing.
+	    private static readonly ILog _log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
 
-		private class StringWriterWithEncoding : StringWriter {
+        // This is a piece of voodoo I was handed by a friend who had some
+        //		similar occasional encoding problems which apparently are
+        //		due to an underlying problem in the BCL.
+        // Find out if its still relevant and what exactly its doing.
+
+        private class StringWriterWithEncoding : StringWriter {
 			private readonly Encoding _encoding;
 
 			public StringWriterWithEncoding(StringBuilder builder, Encoding encoding)
@@ -46,6 +50,7 @@ namespace Inversion.Web.Behaviour.View {
 		private readonly string _contentType;
 		private readonly bool _enableCache;
 	    private readonly bool _disableDirective;
+	    private readonly bool _diagnostics;
 
 		/// <summary>
 		/// Instantiates a new xslt view behaviour used to provide xslt templating
@@ -57,19 +62,34 @@ namespace Inversion.Web.Behaviour.View {
 		/// </remarks>
 		public XsltViewBehaviour(string respondsTo) : this(respondsTo, "text/xml") { }
 
-		/// <summary>
-		/// Instantiates a new xslt view behaviour used to provide xslt templating
-		/// primarily for web applications.
-		/// </summary>
-		/// <param name="respondsTo">The message the behaviour has set as responding to.</param>
-		/// <param name="contentType">The content type of the view step produced from this behaviour.</param>
-		/// <remarks>
-		/// Defaults to caching compiled xslt.
-		/// </remarks>
-		public XsltViewBehaviour(string respondsTo, string contentType)
+	    /// <summary>
+	    /// Instantiates a new xslt view behaviour used to provide xslt templating
+	    /// primarily for web applications.
+	    /// </summary>
+	    /// <param name="respondsTo">The message the behaviour has set as responding to.</param>
+	    /// <param name="enableCache">Specifies whether or not the xslt compilation should be cached.</param>
+	    /// <remarks>Defaults to a content type of "text/xml".</remarks>
+	    public XsltViewBehaviour(string respondsTo, bool enableCache)
+	        : this(respondsTo)
+	    {
+	        _enableCache = enableCache;
+	    }
+
+        /// <summary>
+        /// Instantiates a new xslt view behaviour used to provide xslt templating
+        /// primarily for web applications.
+        /// </summary>
+        /// <param name="respondsTo">The message the behaviour has set as responding to.</param>
+        /// <param name="contentType">The content type of the view step produced from this behaviour.</param>
+        /// <param name="diagnostics">Add diagnostics log output</param>
+        /// <remarks>
+        /// Defaults to caching compiled xslt.
+        /// </remarks>
+        public XsltViewBehaviour(string respondsTo, string contentType, bool diagnostics = false)
 			: base(respondsTo) {
 			_contentType = contentType;
 			_enableCache = true;
+            _diagnostics = diagnostics;
 		}
 
 		/// <summary>
@@ -79,10 +99,11 @@ namespace Inversion.Web.Behaviour.View {
 		/// <param name="respondsTo">The message the behaviour has set as responding to.</param>
 		/// <param name="contentType">The content type of the view step produced from this behaviour.</param>
 		/// <param name="enableCache">Specifies whether or not the xslt compilation should be cached.</param>
-		public XsltViewBehaviour(string respondsTo, string contentType, bool enableCache)
+		public XsltViewBehaviour(string respondsTo, string contentType, bool enableCache, bool diagnostics = false)
 			: this(respondsTo, contentType) {
 			_enableCache = enableCache;
-		}
+		    _diagnostics = diagnostics;
+        }
 
         /// <summary>
 		/// Instantiates a new xslt view behaviour used to provide xslt templating
@@ -92,24 +113,13 @@ namespace Inversion.Web.Behaviour.View {
 		/// <param name="contentType">The content type of the view step produced from this behaviour.</param>
 		/// <param name="enableCache">Specifies whether or not the xslt compilation should be cached.</param>
 		/// <param name="disableDirective">Specifies whether or not to disable the xml directive.</param>
-		public XsltViewBehaviour(string respondsTo, string contentType, bool enableCache, bool disableDirective)
+		public XsltViewBehaviour(string respondsTo, string contentType, bool enableCache, bool disableDirective, bool diagnostics = false)
             : this(respondsTo, contentType)
         {
             _enableCache = enableCache;
             _disableDirective = disableDirective;
+            _diagnostics = diagnostics;
         }
-
-        /// <summary>
-        /// Instantiates a new xslt view behaviour used to provide xslt templating
-        /// primarily for web applications.
-        /// </summary>
-        /// <param name="respondsTo">The message the behaviour has set as responding to.</param>
-        /// <param name="enableCache">Specifies whether or not the xslt compilation should be cached.</param>
-        /// <remarks>Defaults to a content type of "text/xml".</remarks>
-        public XsltViewBehaviour(string respondsTo, bool enableCache)
-			: this(respondsTo) {
-			_enableCache = enableCache;
-		}
 
 		// The iterator generated for this should be
 		//		ThreadLocal and therefore safe to use
@@ -142,7 +152,7 @@ namespace Inversion.Web.Behaviour.View {
 
 		/// <summary>
 		/// Takes the content of the last view-step and transforms it with the xslt with the location
-		/// that best matches the path of the url. 
+		/// that best matches the path of the url.
 		/// </summary>
 		/// <param name="ev">The event that gave rise to this action.</param>
 		/// <param name="context">The context within which this action is being performed.</param>
@@ -163,10 +173,20 @@ namespace Inversion.Web.Behaviour.View {
 
 					if (xsl == null) {
 						// we dont have it cached
-						// does the file exist?					
+						// does the file exist?
 						string templatePath = Path.Combine("Resources", "Views", "Xslt", templateName);
+
+					    if (_diagnostics)
+					    {
+					        _log.DebugFormat("Checking path {0}", templatePath);
+					    }
+
 						if (context.Resources.Exists(templatePath)){
-							xsl = context.Resources.Open(templatePath).AsXslDocument();
+						    if (_diagnostics)
+						    {
+						        _log.DebugFormat("Path {0} exists", templatePath);
+						    }
+                            xsl = context.Resources.Open(templatePath).AsXslDocument();
 							if (_enableCache) {
 								var cacheEntry = context.ObjectCache.CreateEntry(cacheKey);
 								cacheEntry.Value = xsl;
@@ -175,16 +195,20 @@ namespace Inversion.Web.Behaviour.View {
 					}
 					// do the actual transform
 					if (xsl != null) {
-						// copy forward the parameters from the context
-						// to the xsl stylesheet, with no namespace
-						XsltArgumentList args = new XsltArgumentList();
+					    if (_diagnostics)
+					    {
+					        _log.DebugFormat("XSL loaded");
+					    }
+                        // copy forward the parameters from the context
+                        // to the xsl stylesheet, with no namespace
+                        XsltArgumentList args = new XsltArgumentList();
 						foreach (KeyValuePair<string, string> parm in context.Params) {
 							args.AddParam(parm.Key, "", parm.Value);
 						}
 
 						StringBuilder result = new StringBuilder();
 						XmlDocument input = new XmlDocument();
-						
+
 						string inputText = context.ViewSteps.Last.Content ?? context.ViewSteps.Last.Model.ToXml();
 						input.LoadXml(inputText);
 
@@ -199,18 +223,30 @@ namespace Inversion.Web.Behaviour.View {
 					        using (XmlWriter writer = XmlWriter.Create(result, writerSettings))
 					        {
 					            xsl.Transform(input, args, writer);
-					        }
+					            if (_diagnostics)
+					            {
+					                _log.DebugFormat("Transformed");
+					            }
+                            }
 					    }
 					    else
 					    {
 					        using (StringWriterWithEncoding writer = new StringWriterWithEncoding(result, Encoding.UTF8))
 					        {
                                 xsl.Transform(input, args, writer);
+					            if (_diagnostics)
+					            {
+					                _log.DebugFormat("Transformed");
+					            }
                             }
                         }
 
 						context.ViewSteps.CreateStep(templateName, _contentType, result.ToString());
-						break; // we've found and processed our template, no need to keep looking
+					    if (_diagnostics)
+					    {
+					        _log.DebugFormat("ViewStep created");
+					    }
+                        break; // we've found and processed our template, no need to keep looking
 					}
 				}
 
